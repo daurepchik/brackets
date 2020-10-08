3acfd5605914965205814ca122cb777b28f98a95
Aidenov Dauren <daurenblin5@gmail.com>
2020-10-09 00:21:27 +0500

    init: implement the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let brackObj = Object.fromEntries(bracketsConfig);
+
+    let tmp = str.split(``)
+    for (let i in tmp) {
+        if (stack[0] === tmp[i]) {
+            stack.shift();
+        } else if (brackObj[tmp[i]]) {
+            stack.unshift(brackObj[tmp[i]]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
 }
